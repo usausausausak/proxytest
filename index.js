8fc8db1a91535f1420b15c5766fc4bf9158fb07f
usausausausak <usausausausak@gmail.com>
2017-12-09 07:06:07 +0800

    add path param

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var proxy = require('express-http-proxy');
 var app = require('express')();
 
 var root = process.env.PROXY_ROOT || process.exit(1);
+var path = process.env.PROXY_PATH || 'proxy';
 
-app.use('/', proxy(root));
+app.use('/' + path, proxy(root));
+app.use('/', function (req, res) {
+    res.send("It works.");
+});
 app.listen(8080);
