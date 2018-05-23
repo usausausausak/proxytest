0a95450e13262ec1a55174b53c0bdf01786f18c5
usausausausak <usausausausak@gmail.com>
2018-05-23 13:42:06 +0800

    use port from env

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ var app = require('express')();
 
 var root = process.env.PROXY_ROOT || process.exit(1);
 var path = process.env.PROXY_PATH || 'proxy';
+var port = process.env.PORT || 8080;
 
 app.use('/' + path, proxy(root));
 app.use('/', function (req, res) {
     res.send("It works.");
 });
-app.listen(8080);
+app.listen(port);
